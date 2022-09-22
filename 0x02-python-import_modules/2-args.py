30843a1d53a864880fb70926e6c4b4e76ea7ef80
Skynet006 <luckyezealor@gmail.com>
2022-09-22 21:49:02 +0000

    completed

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-from sys import argv
-num_args = len(argv)
-total = 0
-for i in range(1, num_args):
-    total += int(argv[1])
-    print("{:d}".format(total))
+if __name__ == "__main__":
+    from sys import argv
+    num_args = len(argv)
+    total = 0
+    for i in range(1, num_args):
+        total += int(argv[1])
+        print("{:d}".format(total))
