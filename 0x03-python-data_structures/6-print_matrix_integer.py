318869011291e15412d45074959f666d038cbbef
Skynet006 <luckyezealor@gmail.com>
2022-10-19 23:05:34 +0000

    Completed

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for x in range(len(matrix)):
+        if y != 0:
+            print(" ", end='')
+        print("{:d}".format(matrix[x][y]), end='')
+    print()
