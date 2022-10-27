5f984a8509234c99dad93025f71af4618ee67c86
Skynet006 <luckyezealor@gmail.com>
2022-10-27 12:12:52 +0000

    Completed

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 def search_replace(my_list, search, replace):
     """ replaces all occurrences of an element by another in a new list"""
     def s_r_elm(elm):
