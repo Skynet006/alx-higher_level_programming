6033f7e695209991616e95864c13fac34ba1b964
Skynet006 <luckyezealor@gmail.com>
2022-11-02 13:11:33 +0000

    Completed

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Working on oop with python """
 
+
 class Square():
     """Class representing a square"""
 
     def __init__(self, size):
         """ initialize the class
-        
+
         Args:
             size (int): size of the square
         """
