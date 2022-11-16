727f1cd8c53a82c06b2fbff7019b87a8947608d6
Skynet006 <luckyezealor@gmail.com>
2022-11-16 15:14:02 +0000

    Completed

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Defines a class Square"""
+"""
+Defines a class Square
+"""
 
 
 class square:
@@ -8,42 +10,25 @@ class square:
         __size (int): size of a side of the square
     """
     def __init__(self, size=0):
-        """initializes the square
-        Args:
-            size (int): size of a side of the square
-        Returns:
-        None
-        """
-        self.size = size
-
-    def area(self):
-        """calculates the square's area
-        Returns:
-            The area of the square
-        """
-        return (self.__size) ** 2
+        """initializes the square"""
+        self.__size = size
 
     @property
     def size(self):
-        """getter of __szie
-        Returns:
-        The size of the square
-        """
+        """Returns size of the square"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        """setter of __size
-        Args:
-            value (int): the size of a size of the square
-        Returns:
-            None
-        """
+        """sets size to a value"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        else:
-            if value < 0:
-                raise ValueError("size must be >= 0")
-            else:
-                self.__size = value
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the area of a square"""
+        sq_area = self.__size ** 2
+        return sq_area
 
