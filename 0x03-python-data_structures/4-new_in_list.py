3fbab85be2d74375b8939da5ad48e25f18154398
Skynet006 <luckyezealor@gmail.com>
2023-08-14 20:34:43 +0100

    TASK COMPLETED

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    """
+    replaces an element in a list at a specific position
+    without modifying the original list
+    """
+    new_list = my_list[:]
+    length = len(my_list)
+    if idx < 0:
+        return(new_list)
+    if idx > length - 1:
+        return (new_list)
+    new_list[idx] = element
+    return(new_list)
